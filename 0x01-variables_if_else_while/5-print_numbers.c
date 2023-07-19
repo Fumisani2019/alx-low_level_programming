d6ac1977b345bc55ed35d637c0aa2956fe115245
fumisani2019 <fumza89@gmail.com>
2023-07-18 20:49:56 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabet Entry point
+ * Return: Alays 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
