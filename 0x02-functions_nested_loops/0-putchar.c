9efdf2a9d71dc1e3cbb5a5697954422fe26d1cbc
fumisani2019 <fumza89@gmail.com>
2023-07-20 04:51:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print Entry point
+ * Return: Alwys 0 (Success)
+ */
+
+int main(void)
+{
+	printf("putchar\n");
+
+	return (0);
+}
