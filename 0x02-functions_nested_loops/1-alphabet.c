e6b21611041991b543751490a3f777445e434784
fumisani2019 <fumza89@gmail.com>
2023-07-20 06:15:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Prints the lowercase alphabet
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+
+	_putchar(c);
+	c++;
+
+	}
+
+	_putchar('\n');
+
+}
