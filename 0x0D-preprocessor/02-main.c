61269d5812d5383802c91a007896e0c54993f6cf
Fumisani2019 <fumza89@gmail.com>
2023-11-06 05:47:10 -0800

    2-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main()
 {
-	printf("2-main.c\n");
+	printf("%s\n", __FILE__);
 	return (0);
 }
